4a2a55799cb224a5a32388591b3a37b9ae843749
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-19 23:13:32 +0900

    interface 사용하는 이유

---
+++ src/main/java/polymorphism/TVUser.java
@@ -0,0 +1,22 @@
+package polymorphism;
+
+public class TVUser {
+	public static void main(String [] args) {
+		
+		SamsungTV tv = new SamsungTV();
+		
+		tv.powerOn();
+		tv.volumeUp();
+		tv.volumeDown();
+		tv.powerOff();
+		
+		System.out.println();
+		
+		LgTV tv2 = new LgTV();
+		
+		tv2.powerOn();
+		tv2.volumeUp();
+		tv2.volumeDown();
+		tv2.powerOff();
+	}
+}
