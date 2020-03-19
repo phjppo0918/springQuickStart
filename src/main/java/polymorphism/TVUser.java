e57e9a97d02262334d5d17581d7b49cab295dd37
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-20 00:32:48 +0900

    class 02 finish

--- src/main/java/polymorphism/TVUser.java
+++ src/main/java/polymorphism/TVUser.java
@@ -3,20 +3,27 @@ package polymorphism;
 public class TVUser {
 	public static void main(String [] args) {
 		
-		SamsungTV tv = new SamsungTV();
+//		SamsungTV tv = new SamsungTV();
+//		
+//		tv.powerOn();
+//		tv.volumeUp();
+//		tv.volumeDown();
+//		tv.powerOff();
+//		
+//		System.out.println();
+//		
+//		LgTV tv2 = new LgTV();
+//		
+//		tv2.powerOn();
+//		tv2.volumeUp();
+//		tv2.volumeDown();
+//		tv2.powerOff();
 		
+		BeanFactory factory = new BeanFactory();
+		TV tv = (TV)factory.getBean(args[0]);
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
 		tv.powerOff();
-		
-		System.out.println();
-		
-		LgTV tv2 = new LgTV();
-		
-		tv2.powerOn();
-		tv2.volumeUp();
-		tv2.volumeDown();
-		tv2.powerOff();
 	}
 }
