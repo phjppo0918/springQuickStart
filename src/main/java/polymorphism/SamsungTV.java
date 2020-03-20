e8e5e57a48d57f499a3e2679b7c3f6ae44e48fa8
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-20 16:44:24 +0900

    class 03 finish

--- src/main/java/polymorphism/SamsungTV.java
+++ src/main/java/polymorphism/SamsungTV.java
@@ -2,6 +2,21 @@ package polymorphism;
 
 public class SamsungTV implements TV {
 	
+	public SamsungTV() {
+		
+		System.out.println("===> SamsungTV 객체 생성");
+	
+	}
+	
+	public void initMethod() {
+		System.out.println("객체 초기화 작업 처리..");
+	}
+	
+	public void destroyMethod() {
+		System.out.println("객체 삭제 전에 처리한 로직 처리...");
+	}
+	
+	
 	public void powerOn() {
 		System.out.println("SamsungTV---전원 켠다.");
 	}
