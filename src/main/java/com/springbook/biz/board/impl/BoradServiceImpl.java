9e5c1d19f7b3ef67585f115f6e4245aeb575cd68
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-21 12:23:18 +0900

    비즈니스 컴포넌트 실습1 완료!

--- src/main/java/com/springbook/biz/board/impl/BoradServiceImpl.java
+++ src/main/java/com/springbook/biz/board/impl/BoradServiceImpl.java
@@ -1,5 +1,39 @@
 package com.springbook.biz.board.impl;
 
-public class BoradServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springbook.biz.board.BoardDTO;
+import com.springbook.biz.board.BoardService;
+
+@Service("boardService")
+public class BoradServiceImpl implements BoardService {
+	@Autowired
+	private BoardDAO boardDAO;
+	
+	public void insertBoard(BoardDTO dto) {
+		boardDAO.insertBoard(dto);
+		
+	}
+
+	public void updateBoard(BoardDTO dto) {
+		boardDAO.updateBoard(dto);
+		
+	}
+
+	public void deleteBoard(BoardDTO dto) {
+		boardDAO.deleteBoard(dto);
+		
+	}
+
+	public BoardDTO getBoard(BoardDTO dto) {
+		return boardDAO.getBoard(dto);
+	}
+
+	public List<BoardDTO> getBoardList(BoardDTO dto) {
+		return boardDAO.getBoardList(dto);
+	}
 
 }
--- src/test/java/com/springbook/biz/board/BoardServiceClient.java
+++ src/test/java/com/springbook/biz/board/BoardServiceClient.java
@@ -1,5 +1,32 @@
-	package com.springbook.biz.board;
+package com.springbook.biz.board;
+
+import java.util.List;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class BoardServiceClient {
+	public static void main(String[] args) {
+		// 1. Spring 컨테이너를 구동한다.
+		AbstractApplicationContext container = new GenericXmlApplicationContext("applicationContextBoardService.xml");
 
+		// 2. Spring 컨테이너로부터 BoardServiceImpl 객체를 lookup 한다.
+		BoardService boardService = (BoardService) container.getBean("boardService");
+
+		// 3. 글 등록 기능 테스트
+		BoardDTO dto = new BoardDTO();
+		dto.setTitle("임시 제목2");
+		dto.setWriter("관리자");
+		dto.setContent("잘 부탁드립니당~");
+		boardService.insertBoard(dto);
+
+		// 4. 글 목록 검색 기능 테스트
+		List<BoardDTO> boardList = boardService.getBoardList(dto);
+		for (BoardDTO board : boardList) {
+			System.out.println("--->" + board.toString());
+		}
+		
+		//5. Spring 컨테이너 종료
+		container.close();
+	}
 }
