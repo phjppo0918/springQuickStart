e36ca0592f8a42e245a323bcff351714a3cc7a09
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-21 13:22:11 +0900

    userService annotaion 처리

--- src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
+++ src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.springbook.biz.user.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.springbook.biz.user.UserDTO;
 import com.springbook.biz.user.UserService;
 
+@Service("userService")
 public class UserServiceImpl implements UserService {
-
+	@Autowired
 	private UserDAO userDAO;
 	
 	
