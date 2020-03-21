669657decf66cb5bd3370182e916366a9c6e57d9
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-21 11:44:29 +0900

    BoardService까지 구현 완료

--- src/main/java/com/springbook/biz/board/BoardService.java
+++ src/main/java/com/springbook/biz/board/BoardService.java
@@ -1,5 +1,23 @@
 package com.springbook.biz.board;
 
-public class BoardService {
+import java.util.List;
 
+public interface BoardService {
+	
+	//CRUD 기능의 메소드 구현
+	//글 등록
+	void insertBoard(BoardDTO dto);
+	
+	//글 수정
+	void updateBoard(BoardDTO dto);
+	
+	//글 삭제
+	void deleteBoard(BoardDTO dto);
+	
+	//글 상세 조회
+	BoardDTO getBoard(BoardDTO dto);
+	
+	//글 목록 조회
+	List<BoardDTO> getBoardList(BoardDTO dto);
+	
 }
--- src/main/java/com/springbook/biz/board/impl/BoardDAO.java
+++ src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -3,6 +3,8 @@ package com.springbook.biz.board.impl;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
@@ -43,7 +45,94 @@ public class BoardDAO {
 			JDBCUtil.close(stmt, conn);
 		}
 	}
-	
-	//글 수정
-	
+
+	// 글 수정
+	public void updateBoard(BoardDTO dto) {
+		System.out.println("===> JDBC로 updateBoard() 기능 처리");
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_UPDATE);
+			stmt.setString(1, dto.getTitle());
+			stmt.setString(2, dto.getContent());
+			stmt.setInt(3, dto.getSeq());
+			stmt.executeUpdate();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(stmt, conn);
+		}
+	}
+
+	// 글 삭제
+	public void deleteBoard(BoardDTO dto) {
+		System.out.println("===> JDBC로 deleteBoard() 기능 처리");
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_DELETE);
+			stmt.setInt(1, dto.getSeq());
+			stmt.executeUpdate();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(stmt, conn);
+		}
+	}
+
+	// 글 상세 조회
+	public BoardDTO getBoard(BoardDTO dto) {
+		System.out.println("===> JDBC로 getBoard() 기능 처리");
+		BoardDTO board = null;
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_GET);
+			stmt.setInt(1, dto.getSeq());
+			rs = stmt.executeQuery();
+			if (rs.next()) {
+				board = new BoardDTO();
+				board.setSeq(rs.getInt("SEQ"));
+				board.setTitle(rs.getString("TITLE"));
+				board.setWriter(rs.getString("WRITER"));
+				board.setContent(rs.getString("CONTENT"));
+				board.setRegDate(rs.getDate("REGDATE"));
+				board.setCnt(rs.getInt("CNT"));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(rs, stmt, conn);
+		}
+		return board;
+	}
+
+	// 글 목록 조회
+	public List<BoardDTO> getBoardList(BoardDTO dto) {
+		System.out.println("===> JDBC로 getBoardList() 기능 처리");
+		List<BoardDTO> boardList = new ArrayList<BoardDTO>();
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_LIST);
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+				BoardDTO board = new BoardDTO();
+				board.setSeq(rs.getInt("SEQ"));
+				board.setTitle(rs.getString("TITLE"));
+				board.setWriter(rs.getString("WRITER"));
+				board.setContent(rs.getString("CONTENT"));
+				board.setRegDate(rs.getDate("REGDATE"));
+				board.setCnt(rs.getInt("CNT"));
+				boardList.add(board);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(rs, stmt, conn);
+		}
+		return boardList;
+	}
 }
