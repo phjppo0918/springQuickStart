ba1b5874d6e2209457cf9a1f36ab5887db0f9bfc
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-26 17:18:07 +0900

    서블릿 구현

--- src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -35,6 +35,16 @@ public class DispatcherServlet extends HttpServlet {
 			System.out.println("로그인 처리");
 		}else if(path.equals("/logout.do")) {
 			System.out.println("로그아웃 처리");
+		}else if(path.equals("/insertBoard.do")) {
+			System.out.println("글 등록 처리");
+		}else if(path.equals("/updateBoard.do")) {
+			System.out.println("글 수정 처리");
+		}else if(path.equals("/deleteBoard.do")) {
+			System.out.println("글 삭제 처리");
+		}else if(path.equals("/getBoard.do")) {
+			System.out.println("글 상제 조회 처리");
+		}else if(path.equals("/getBoardList.do")) {
+			System.out.println("글 목록 검색 처리");
 		}
 	
 	}	
