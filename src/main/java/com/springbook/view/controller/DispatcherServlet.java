6c6ee0e66e176fe4a7a703ca1e02a81ea2d904de
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-26 17:53:05 +0900

    board.jsp 까지 서블릿 완료

--- src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -1,51 +1,107 @@
 package com.springbook.view.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.springbook.biz.board.BoardDTO;
+import com.springbook.biz.board.impl.BoardDAO;
+import com.springbook.biz.user.UserDTO;
+import com.springbook.biz.user.impl.UserDAO;
 
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
- 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		process(request,response);
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		process(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		process(request, response);
 	}
-	
-	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	private void process(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// 1. 클라이언트의 요청 path 정보를 추출한다
 		String uri = request.getRequestURI();
 		String path = uri.substring(uri.lastIndexOf("/"));
 		System.out.println(path);
-	
-		//2. 클라이언트의 요청 path에 따라 적절히 분가처리 한다.
-		if(path.equals("/login.do")) {
+
+		// 2. 클라이언트의 요청 path에 따라 적절히 분가처리 한다.
+		if (path.equals("/login.do")) {
 			System.out.println("로그인 처리");
-		}else if(path.equals("/logout.do")) {
+
+			// 1. 사용자 입력 정보 추출
+			String id = request.getParameter("id");
+			String password = request.getParameter("password");
+
+			// 2. DB 연동 처리
+			UserDTO dto = new UserDTO();
+			dto.setId(id);
+			dto.setPassword(password);
+
+			UserDAO userDAO = new UserDAO();
+			UserDTO user = userDAO.getUser(dto);
+
+			// 3. 화면 네비게이션
+			if (user != null) {
+				response.sendRedirect("getBoardList.do");
+			} else {
+				response.sendRedirect("login.jsp");
+			}
+
+		} else if (path.equals("/logout.do")) {
 			System.out.println("로그아웃 처리");
-		}else if(path.equals("/insertBoard.do")) {
+		} else if (path.equals("/insertBoard.do")) {
 			System.out.println("글 등록 처리");
-		}else if(path.equals("/updateBoard.do")) {
+		} else if (path.equals("/updateBoard.do")) {
 			System.out.println("글 수정 처리");
-		}else if(path.equals("/deleteBoard.do")) {
+		} else if (path.equals("/deleteBoard.do")) {
 			System.out.println("글 삭제 처리");
-		}else if(path.equals("/getBoard.do")) {
+		} else if (path.equals("/getBoard.do")) {
 			System.out.println("글 상제 조회 처리");
-		}else if(path.equals("/getBoardList.do")) {
+			
+			// 1. 검색할 게시글 번호 추출
+			String seq = request.getParameter("seq");
+
+			// 2. DB 연동 처리
+			BoardDTO dto = new BoardDTO();
+			dto.setSeq(Integer.parseInt(seq));
+			
+			BoardDAO boardDAO = new BoardDAO();
+			BoardDTO board = boardDAO.getBoard(dto);
+			
+			HttpSession session = request.getSession();
+			session.setAttribute("board", board);
+			response.sendRedirect("getBoard.jsp");
+		} else if (path.equals("/getBoardList.do")) {
 			System.out.println("글 목록 검색 처리");
+
+			// 1. 사용자 입력 정보 추출(검색기능은 나중에 구현)
+			// 2. DB 연동 처리
+			BoardDTO dto = new BoardDTO();
+			BoardDAO boardDAO = new BoardDAO();
+			List<BoardDTO> boardList = boardDAO.getBoardList(dto);
+
+			// 3. 검색 결과는 세션에 저장하고 목록 화면으로 이동한다.
+			HttpSession session = request.getSession();
+			session.setAttribute("boardList", boardList);
+			response.sendRedirect("getBoardList.jsp");
+
 		}
-	
-	}	
+
+	}
 }
