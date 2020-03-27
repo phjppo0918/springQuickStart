b1eaba687d283700ab731df154d05ecb75b1853d
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-27 19:43:34 +0900

    update model2 fin

--- src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -86,6 +86,25 @@ public class DispatcherServlet extends HttpServlet {
 			
 		} else if (path.equals("/updateBoard.do")) {
 			System.out.println("글 수정 처리");
+			
+			//1. 사용자 입력 정보 추출
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			String seq = request.getParameter("seq");
+			
+			//2. DB 연동 처리
+			BoardDTO dto = new BoardDTO();
+			dto.setTitle(title);
+			dto.setContent(content);
+			dto.setSeq(Integer.parseInt(seq));
+			
+			BoardDAO boardDAO = new BoardDAO();
+			boardDAO.updateBoard(dto);
+			
+			//3. 화면 네비게이션
+			response.sendRedirect("getBoardList.do");
+			
+			
 		} else if (path.equals("/deleteBoard.do")) {
 			System.out.println("글 삭제 처리");
 		} else if (path.equals("/getBoard.do")) {
