9b8862dde2d9a8f5106c5eaeeb3b3d5c1084e0e4
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-30 18:38:14 +0900

    SpringMVC update.do,delete.do, logout.do fin

--- src/main/java/com/springbook/view/springMVC/board/DeleteBoardController.java
+++ src/main/java/com/springbook/view/springMVC/board/DeleteBoardController.java
@@ -3,14 +3,16 @@ package com.springbook.view.springMVC.board;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
 import com.springbook.biz.board.BoardDTO;
 import com.springbook.biz.board.impl.BoardDAO;
-import com.springbook.view.springMVC.controller.Controller;
 
 public class DeleteBoardController implements Controller {
 
 	@Override
-	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		System.out.println("글 삭제 처리");
 
 		// 1. 사용자 입력 정보 추출
@@ -24,7 +26,9 @@ public class DeleteBoardController implements Controller {
 		boardDAO.deleteBoard(dto);
 
 		// 3. 화면 네비게이션
-		return "getBoardList.do";
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("getBoardList.do");
+		return mav;
 
 	}
 
