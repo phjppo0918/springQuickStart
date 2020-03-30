fa4cd178c795b83770d288efd2b1fe9850a9c434
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-30 22:33:18 +0900

    method 에 따라 변경되는 RequestMapping

--- src/main/java/com/springbook/view/springMVCAnnotation/user/LoginController.java
+++ src/main/java/com/springbook/view/springMVCAnnotation/user/LoginController.java
@@ -2,16 +2,25 @@ package com.springbook.view.springMVCAnnotation.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.springbook.biz.user.UserDTO;
 import com.springbook.biz.user.impl.UserDAO;
 
 @Controller
 public class LoginController {
-
-	@RequestMapping("/model2/login.do")
+	
+	@RequestMapping(value = "/model2/login.do", method = RequestMethod.GET)
+	public String loginView(UserDTO dto) {
+		System.out.println("로그인 화면으로 이동");
+		dto.setId("test");
+		dto.setPassword("test123");
+		return "login.jsp";
+	}
+	
+	@RequestMapping(value = "/model2/login.do", method = RequestMethod.POST)
 	public String login(UserDTO dto, UserDAO userDAO) {
-		System.out.println("로그인 처리");
+		System.out.println("로그인 인증 처리...");
 
 		if (userDAO.getUser(dto) != null) {
 			return "getBoardList.do";
