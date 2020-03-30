fdd264bae841caf894a9489182edb42520801281
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-30 21:48:50 +0900

    insert and getList Annotaion fin

--- src/main/java/com/springbook/view/springMVCAnnotation/board/InsertBoardController.java
+++ src/main/java/com/springbook/view/springMVCAnnotation/board/InsertBoardController.java
@@ -1,39 +1,24 @@
 package com.springbook.view.springMVCAnnotation.board;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springbook.biz.board.BoardDTO;
 import com.springbook.biz.board.impl.BoardDAO;
 
+@Controller
+public class InsertBoardController {
 
-public class InsertBoardController implements Controller {
-
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	@RequestMapping(value="/model2/insertBoard.do")
+	public String insertBoard(BoardDTO dto, BoardDAO boardDAO) {
 		System.out.println("글 등록 처리");
 
-		// 1. 사용자 입력 정보 추출
-		String title = request.getParameter("title");
-		String writer = request.getParameter("writer");
-		String content = request.getParameter("content");
-
-		// 2. DB 연동 처리
-		BoardDTO dto = new BoardDTO();
-		dto.setTitle(title);
-		dto.setWriter(writer);
-		dto.setContent(content);
-
-		BoardDAO boardDAO = new BoardDAO();
+	
+		// 2. DB 연동 처리		
 		boardDAO.insertBoard(dto);
-
-		// 3. 화면 네비게이션
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("redirect:getBoardList.do");
-		return mav;
+		return "getBoardList.do";
 	}
 
 }
