cba4757c4bc4c530c6d804e4afe79af84fcd2739
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-04-03 14:48:56 +0900

    ERROR 페이지 경로 수정, xml 기반 에러페이지  설정

--- src/main/java/com/springbook/view/springMVCAnnotation/common/CommonExceptionHandler.java
+++ src/main/java/com/springbook/view/springMVCAnnotation/common/CommonExceptionHandler.java
@@ -1,17 +1,18 @@
-package com.springbook.biz.common;
+package com.springbook.view.springMVCAnnotation.common;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
-@ControllerAdvice("com.springboook.view")
+//xml 기반 예외처리를 위해 임시적으로 주석처리
+//@ControllerAdvice("com.springbook.view.springMVCAnnotation")
 public class CommonExceptionHandler {
 	
 	@ExceptionHandler(ArithmeticException.class)
 	public ModelAndView handleArithmeticException(Exception e) {
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("exception",e);
-		mav.setViewName("../../common/arithmeticError.jsp");
+		mav.setViewName("../common/arithmeticError.jsp");
 		return mav;
 	} 
 
@@ -19,7 +20,7 @@ public class CommonExceptionHandler {
 	public ModelAndView handleNullPointerException(Exception e) {
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("exception", e);
-		mav.setViewName("../../common/nullPointerError.jsp");
+		mav.setViewName("../common/nullPointerError.jsp");
 		return mav;
 	}
 	
@@ -27,7 +28,7 @@ public class CommonExceptionHandler {
 	public ModelAndView handleException(Exception e) {
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("exception",e);
-		mav.setViewName("../../common/error.jsp");
+		mav.setViewName("../common/error.jsp");
 		return mav;
 	}
 }
